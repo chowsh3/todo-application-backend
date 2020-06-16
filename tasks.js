a7c675119e8936d342cf175ea3509c51d5c8aeb9
chowsh3 <shobir.c@icloud.com>
2020-06-17 00:19:24 +0100

    completed put, post, get, delete

--- tasks.js
+++ tasks.js
@@ -6,11 +6,97 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/tasks", function(req, res) {
-  res.send({ 
-    tasks: ["water plants", "do dishes", "buy oats"] 
+app.get("/tasks", function (req, res) {
+  
+
+  const someTasks = [
+    {
+    tasks: "water plants", 
+    completed: false,
+    date: "2020-06-20",
+    taskId: 1
+    },
+    {
+    tasks: "buy bread", 
+    completed: false,
+    date: "2020-06-20",
+    taskId: 2
+   },
+   {
+    tasks: "complete tasks", 
+    completed: false,
+    date: "2020-06-20",
+    taskId: 3
+    }
+  ];
+
+  res.status(200).send(someTasks);
+});
+
+app.post("/tasks", function (req, res) {
+  const text = req.body.text;
+  const date = req.body.date;
+
+  res.status(201).json({
+    message: `Received a request to add task ${text} with date ${date}`
   });
 });
+
+app.put("/tasks/:taskId", function (req, res) {
+  
+  const id = req.params.taskId;
+
+  res.status(200).json({
+    message: "You issued a put request for ID: " + id
+  });
+});
+
+app.delete("/tasks/:taskId", function(req, res) {
+  
+  const someTasks = [
+    {
+    tasks: "water plants", 
+    completed: false,
+    date: "2020-06-20",
+    taskId: 1
+    },
+    {
+    tasks: "buy bread", 
+    completed: false,
+    date: "2020-06-20",
+    taskId: 2
+   },
+   {
+    tasks: "complete tasks", 
+    completed: false,
+    date: "2020-06-20",
+    taskId: 3
+    }
+  ];
+  const id = req.params.taskId;
+
+
+
+  let highestIdValues = someTasks.map(obj => obj.taskId);
+  let highestID = Math.max(...highestIdValues);
+
+  let aResponse = {
+    message: " You issued a delete request for ID: " + id
+  };
+
+  if(id>highestID) {
+    res.status(404);
+      aResponse = {
+        message: "task " + id + " does not exist"
+      };
+    
+  }
+
+  res.json(aResponse);
+
+});
+
 
 module.exports.handler = serverless(app);
